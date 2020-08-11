3d7da92e2c9e1822eaaa23a02d56aea4fb4dd940
Rishabh PC <rishabhsinha220@gmail.com>
2020-08-11 12:51:57 +0530

    Adding new user for database use

--- Backend/config.py
+++ Backend/config.py
@@ -5,4 +5,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost/productivity_app"
+SQLALCHEMY_DATABASE_URI = "postgresql://development1:database123@localhost/productivity_app"
